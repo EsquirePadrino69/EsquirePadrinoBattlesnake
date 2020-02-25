77dca862444d2eb88f582a640f6e370b74eabeb9
Riley Carter <riley_carter@outlook.com>
2020-02-24 19:51:58 -0800

    trynna move

--- app/logic.py
+++ app/logic.py
@@ -1,26 +1,3 @@
 import sys
 def get_move(data):
-	board=[[0 for i in range (data['board']['height'])] for i in range(data['board']['width'])]
-
-
-	for food in data['board']['food']:
-		x=food['x'] 
-		y=food['y']
-		board[x][y]=1
-
-	for x in range(len(board)):
-		for y in range(len(board[0])):
-			sys.stdout.write(str(board[x][y]))
-		sys.stdout.write('\n')
-
-
-
- get_move()
- turn_left()
- get_move()
- turn_left()
- get_move()
- turn_left()
- get_move()
- turn_left()    
- 
+	return "left"
