e8f94d20aecdf4937e53d60aac6b7cd757fd1444
Riley Carter <riley_carter@outlook.com>
2020-02-25 20:14:53 -0800

    trying my own code

--- app/logic.py
+++ app/logic.py
@@ -4,18 +4,18 @@ def get_move(data):
 	head = ['you']['body']
 	x = head['x']
 	y = head['y']
-	left = (x-1, y)
-	up = (x, y-1)
-	down = (x, y+1)
-	right = (x+1, y)
+	left = x-1, y
+	up = x, y-1
+	down = x, y+1
+	right = x+1, y
 
 	("snake") = (p)
-	if (p) is (x-1, y)
+	if (p) is x-1, y
 		else: right
-	if (p) is (x+1, y)
+	if (p) is x+1, y
 		else: left
-	if (p) is (x, y-1)
+	if (p) is x, y-1
 		else: down
-	if (p) is (x, y+1)
+	if (p) is x, y+1
 		else: up
 		
